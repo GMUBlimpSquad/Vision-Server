a58be981531b0d408a2d438186783cba48845cee
Super-Yojan <gautamyojan0@gmail.com>
2024-03-08 08:14:50 -0500

    added nginx and docker compose and run script to make it scable

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-
-docker rm vision-server-container
-docker run -p 8000:8000 -d -v .:/home/ --name vision-server-container vision-server
+sudo docker compose up --scale vision=5 -d
--- src/main.py
+++ src/main.py
@@ -14,20 +14,21 @@ PORT = 8000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen()
-conn, addr = s.accept()
 
 frameSize = 0
 model = torch.hub.load("ultralytics/yolov5", 'custom', path="model/best.pt", force_reload=True)
 
 
-try:
-    while (True):
+def on_new_client(conn, addr):
+    print("Connected to ",addr)
+    while True:
         header = conn.recv(4)
         try:
             bytes_to_read = int(header.decode())
         except:
             bytes_to_read = 0
         img = bytes()
+        #print(bytes_to_read)
         while len(img) < bytes_to_read:
             img += conn.recv(min(bytes_to_read - len(img), 4096))
 
@@ -51,8 +52,15 @@ try:
         else:
             print("Bad frame recv")
 
+    conn.close()
 
-        if cv.waitKey(1) == ord("q"):
-            break
+
+
+
+try:
+    while (True):
+        conn, addr = s.accept()
+        thread.start_new_thread(on_new_client, (c,addr))
+
 except KeyboardInterrupt:
     s.close()
